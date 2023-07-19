c86086e99bd9fdbe3fb87c9aa4a2c85c3e28e4d6
Nicholas Charriere <nicholascharriere@gmail.com>
2023-07-19 14:56:06 -0700

    Increase LSP timeout

--- init.lua
+++ init.lua
@@ -30,9 +30,9 @@ return {
       -- control auto formatting on save
       format_on_save = {
         enabled = true,     -- enable or disable format on save globally
-        allow_filetypes = { -- enable format on save for specified filetypes only
-          -- "go",
-        },
+        -- allow_filetypes = { -- enable format on save for specified filetypes only
+        --   -- "go",
+        -- },
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
         },
@@ -41,7 +41,7 @@ return {
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
       },
-      timeout_ms = 1000, -- default format timeout
+      timeout_ms = 3500, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
       --   return true
       -- end
